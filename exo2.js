8ac971c36598bd78ee757891b303357cddfbf811
audemar <audemarsadish@gmail.com>
2021-11-18 16:59:18 +0100

    exo2 terminé

---
+++ exo2.js
@@ -0,0 +1,15 @@
+function Val() {
+    let numb1 = pass.value;
+    let numb2 = pass2.value;
+
+console.log(numb1)
+    if (numb1== numb2){
+       pass.className = "true";
+        pass2.className = "true";
+
+    } else {
+        pass.className = "false";
+        pass2.className = "false";
+
+    }
+}
